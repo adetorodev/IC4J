8b8768afd54cecfd302d274eeb4436e65431ea8e
Black_Vamps5 <investormosley9@gmail.com>
2023-01-11 11:55:22 +0100

    fixed: variable

--- JavaScript/Week1/index.js
+++ JavaScript/Week1/index.js
@@ -1,9 +1,11 @@
 let rootNode = document.getElementById("demo")
 
+var langusge = "Javascript"
+
 rootNode.innerHTML = innerHTML = `
 <div>
 <h1> Welcome </h1>
-<h2> This is a refresher class in JavaScript </h2>
+<h2> This is a refresher class in ${langusge} </h2>
 </div>
 `;
 
