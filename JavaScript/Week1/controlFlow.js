9fd04fde1bf555a35c83cf1ef732dcce5669346c
Ezekiel Adetoro <adetoroe787@gmail.com>
2023-01-12 11:49:50 +0100

    fixed: control flow

---
+++ JavaScript/Week1/controlFlow.js
@@ -0,0 +1,47 @@
+//  Control Flow
+// Change and choices
+
+// From Abere to Estate
+//  a bike or Bus
+//  if bike {
+//     estate
+// }
+
+// if Bus {
+//     if ilesaGarage {
+//         olaiya
+//     } else if ayetoro {
+//         if olaiya {
+//             estate
+//         } else if oldGarage{
+//                 estate
+//         }
+//     }
+//     else{
+//         estate
+//     }
+// }
+
+// let year = prompt("Nigeria indenedence is what year? ")
+
+// // if (year == 1960) alert("You are right")
+
+// if (year == 1960) {
+//     alert("You are right")
+// }else {
+//     alert("You are wrong, it is 1960")
+// }
+
+let year = prompt(
+  "In which year was the ECMAScript-2015 specification published?",
+  ""
+);
+if (year < 2015) {
+  alert("Too early...");
+} else if (year > 2015) {
+  alert("Too late");
+} else {
+  alert("Exactly!");
+}
+
+
