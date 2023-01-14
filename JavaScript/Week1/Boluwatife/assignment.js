4482bf8a48d138486aecc614f75a3f08d5b967d7
Black_Vamps5 <investormosley9@gmail.com>
2023-01-14 12:30:21 +0100

    fixed: assignments done

---
+++ JavaScript/Week1/Boluwatife/assignment.js
@@ -0,0 +1,27 @@
+let name = "John"
+
+let admin = name;
+name = "John";
+admin = name;
+
+console.log(admin); //John
+
+let ourPlanetNamee = "Earth";
+
+let bets = "crown:0, anchor:0, heart:0, spade:0, club:0, diamond:0";
+let totalBets = ("totalBet = rand(1, funds)")
+{
+    if(totalBet === '7?'){
+    totalBet = "funds";
+    }else{
+     totalBet = "randomly distribte totalBet";
+    }else if(totalBet = "funds"){
+     bets("heart") = totalBet{
+     }else{
+        "randomly distribte totalBet";
+     }
+    }
+    
+}
+
+"randomly distribte totalBet"
--- JavaScript/Week1/index.js
+++ JavaScript/Week1/index.js
@@ -1,12 +1,47 @@
 let rootNode = document.getElementById("demo")
 
-var langusge = "Javascript"
+// var langusge = "Javascript"
 
-rootNode.innerHTML = innerHTML = `
-<div>
-<h1> Welcome </h1>
-<h2> This is a refresher class in ${langusge} </h2>
-</div>
-`;
+// rootNode.innerHTML = innerHTML = `
+// <div>
+// <h1> Welcome </h1>
+// <h2> This is a refresher class in ${langusge} </h2>
+// </div>
+// `;
+
+// write a program that allow only user below 18 years in our website 
+// Tip: create a Global variable call accessAllow
+// create a variable that holds the user's age
+// let accessAllow;
+
+// let age = prompt("How are you?")
+
+// if(age < 18){
+//     accessAllow = "you are welcome"
+//     } else {
+//         accessAllow = "not welcome"
+//     }
+//     alert(accessAllow)
+
+// let message;
+
+// let login = prompt("Fill in this form")
 
 
+// if(login = "admin"){
+//     message = "hi";
+
+// }else if(login = "worker"){
+//     message = "hello";
+// }else if(login = ""){
+//     message = "no login";
+// }
+// alert(message)
+// let message = 
+// login = "Employee" 
+// ? alert("Hello") 
+// : login = "Director" 
+// ? alert("Greeting") 
+// : login = "" 
+// ? alert("No login") 
+// : "";
