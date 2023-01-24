e8994bd7697deefb1dc814a176e614b332979941
Black_Vamps5 <investormosley9@gmail.com>
2023-01-24 21:36:52 +0100

    done

--- JavaScript/Week1/Boluwatife/assignment.js
+++ JavaScript/Week1/Boluwatife/assignment.js
@@ -24,4 +24,9 @@ let totalBets = ("totalBet = rand(1, funds)")
     
 }
 
+let num;
+
+do {
+  num = prompt("Enter a number greater than 100?", 0);
+} while (num <= 100 && num);
 "randomly distribte totalBet"
